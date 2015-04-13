1dea294f4b8037e15d23d13d6765507478c47151
Ivan von Nagy <ivan@vonnagy.net>
2015-04-12 23:07:11 -0700

    Added Heroku Headers

---
+++ src/main/scala/logreceiver/package.scala
@@ -0,0 +1,35 @@
+import spray.http.HttpHeaders.RawHeader
+import spray.http.{HttpHeaders, MediaType, MediaTypes}
+import spray.routing.directives.HeaderDirectives.headerValuePF
+
+/**
+ * Created by ivannagy on 4/9/15.
+ */
+package object logreceiver {
+
+  val `application/logplex-1` = MediaTypes.register(MediaType.custom("application", "logplex-1", true, false, Nil, Map.empty))
+
+  val `Logplex-Msg-Count` = "Logplex-Msg-Count"
+  val `Logplex-Frame-Id` = "Logplex-Frame-Id"
+  val `Logplex-Drain-Token` = "Logplex-Drain-Token"
+
+  val logplexContentLength =
+    headerValuePF {
+      case HttpHeaders.`Content-Length`(length) => length
+    }
+
+  val logplexMsgCount =
+    headerValuePF {
+      case RawHeader(`Logplex-Msg-Count`, count) => count.toInt
+    }
+
+  val logplexFrameId =
+    headerValuePF {
+      case RawHeader(`Logplex-Frame-Id`, id) => id
+    }
+
+  val logplexToken =
+    headerValuePF {
+      case RawHeader(`Logplex-Drain-Token`, token) => token
+    }
+}
